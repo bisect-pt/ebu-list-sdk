3dfd25b3b950e00027c6b97a2d2cfa493716cfa5
mariana <mariana@bisect.pt>
2021-02-25 18:29:19 +0000

    Added api funcionalities to obtain mp3 url for audio player

--- lib/src/stream.ts
+++ lib/src/stream.ts
@@ -9,6 +9,7 @@ export default class Stream {
         this.transport = transport;
     }
 
+    //Video
     public async getFramesFromStream(pcapID: string, streamID: string | undefined): Promise<IFrameInfo[]> {
         const response = await this.transport.get(`/api/pcap/${pcapID}/stream/${streamID}/frames`);
         const frames: IFrameInfo[] = response as IFrameInfo[];
@@ -17,5 +18,20 @@ export default class Stream {
 
     public getUrlForFrame(pcapID: string, streamID: string | undefined, timestamp: string) {
         return this.transport.rest.getAuthUrl(`/api/pcap/${pcapID}/stream/${streamID}/frame/${timestamp}/png`);
+    }
+
+    //Audio
+    public async renderMp3(pcapID: string, streamID: string | undefined, channelsString: string) {
+        const response = await this.transport.get(
+            `/api/pcap/${pcapID}/stream/${streamID}/rendermp3?channels=${channelsString}`
+        );
+        const render: any = response;
+        return render;
+    }
+
+    public downloadMp3Url(pcapID: string, streamID: string | undefined, channelsString?: string) {
+        return this.transport.rest.getAuthUrl(
+            `/api/pcap/${pcapID}/stream/${streamID}/downloadmp3${channelsString ? `?channels=${channelsString}` : ''}`
+        );
     }
 }
