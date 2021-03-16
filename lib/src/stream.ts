26d496f8f924de7a9e7de3dee8719039dfb6006b
mariana <mariana@bisect.pt>
2021-03-16 12:24:21 +0000

    Added funcionality to fetch api data about rtp line charts

--- lib/src/stream.ts
+++ lib/src/stream.ts
@@ -20,7 +20,7 @@ export default class Stream {
         return this.transport.rest.getAuthUrl(`/api/pcap/${pcapID}/stream/${streamID}/frame/${timestamp}/png`);
     }
 
-    //Video Graphs
+    //Video Graphs - Cinst Line Chart
     public async getCInstForStream(
         pcapID: string,
         streamID: string | undefined,
@@ -34,6 +34,7 @@ export default class Stream {
         return cinstData;
     }
 
+    //Video Graphs - Vrx Line Chart
     public async getVrxIdealForStream(
         pcapID: string,
         streamID: string | undefined,
@@ -48,19 +49,61 @@ export default class Stream {
         const vrxData: any = response;
         return vrxData;
     }
+
+    //Video Graphs - FTP Line Chart
     public async getDeltaToIdealTpr0Raw(
         pcapID: string,
         streamID: string | undefined,
         fromNs: string | undefined,
-        toNs: string | undefined,
-        groupByNanoseconds?: any
+        toNs: string | undefined
     ): Promise<any> {
-        const groupBy = groupByNanoseconds ? `&groupByNanoseconds=${groupByNanoseconds}` : '';
         const response = await this.transport.get(
             `/api/pcap/${pcapID}/stream/${streamID}/analytics/DeltaToIdealTpr0Raw?from=${fromNs}&to=${toNs}`
         );
-        const vrxData: any = response;
-        return vrxData;
+        const ftpData: any = response;
+        return ftpData;
+    }
+
+    //Video Graphs - RTP Latency Line Chart
+    public async getDeltaPacketTimeVsRtpTimeRaw(
+        pcapID: string,
+        streamID: string | undefined,
+        fromNs: string | undefined,
+        toNs: string | undefined
+    ): Promise<any> {
+        const response = await this.transport.get(
+            `/api/pcap/${pcapID}/stream/${streamID}/analytics/DeltaPacketTimeVsRtpTimeRaw?from=${fromNs}&to=${toNs}`
+        );
+        const latencyData: any = response;
+        return latencyData;
+    }
+
+    //Video Graphs - RTP Offset Line Chart
+    public async getDeltaRtpVsNtRaw(
+        pcapID: string,
+        streamID: string | undefined,
+        fromNs: string | undefined,
+        toNs: string | undefined
+    ): Promise<any> {
+        const response = await this.transport.get(
+            `/api/pcap/${pcapID}/stream/${streamID}/analytics/DeltaRtpVsNt?from=${fromNs}&to=${toNs}`
+        );
+        const rtpOffsetData: any = response;
+        return rtpOffsetData;
+    }
+
+    //Video Graphs - RTP Time Step Line Chart
+    public async getDeltaToPreviousRtpTsRaw(
+        pcapID: string,
+        streamID: string | undefined,
+        fromNs: string | undefined,
+        toNs: string | undefined
+    ): Promise<any> {
+        const response = await this.transport.get(
+            `/api/pcap/${pcapID}/stream/${streamID}/analytics/DeltaToPreviousRtpTsRaw?from=${fromNs}&to=${toNs}`
+        );
+        const rtpOffsetData: any = response;
+        return rtpOffsetData;
     }
 
     //Audio
